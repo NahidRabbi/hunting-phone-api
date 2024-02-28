683046da03d9ad6dc633118fa0a41d90fba82687
nahid <nahidrabbi045@gmail.com>
2024-02-29 03:57:21 +0600

    added show all button

--- js/phone.js
+++ js/phone.js
@@ -1,14 +1,14 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
         const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
         const data = await res.json();
         const phones = data.data;
         // console.log(phones);
-        displayPhones(phones);
+        displayPhones(phones, isShowAll);
 }
 
 
-const displayPhones = phones =>{
-     //    console.log(phones)
+const displayPhones = (phones, isShowAll) =>{
+        console.log(phones)
 
      const phoneContainer = document.getElementById('phone-container');
     //  clear phone container cards before adding new cards
@@ -16,19 +16,21 @@ const displayPhones = phones =>{
 
     // display show all button if there are more than 12 phones
     const showAllContainer = document.getElementById('show-all-container')
-    if(phones.length > 12){
+    if(phones.length > 12 && !isShowAll){
        showAllContainer.classList.remove('hidden')
     }
     else{
         showAllContainer.classList.add('hidden');
     }
-
-    // display only first 12 phones
-    phones = phones.slice(0,12);
+     console.log('is show all', isShowAll)
+    // display only first 12 phones if not show all
+    if(!isShowAll){
+        phones = phones.slice(0,12);
+    }
 
 
      phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         // 2 create a div
         const phoneCard = document.createElement('div');
         phoneCard.classList = `card  bg-base-100 p-4 shadow-xl`;
@@ -53,21 +55,20 @@ const displayPhones = phones =>{
 }
 
 // handle search button
-const handleSearch = () =>{
+const handleSearch = (isShowAll) =>{
     toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-    loadPhone(searchText);
+    loadPhone(searchText, isShowAll);
 }
 // handle search recap
-const handleSearch2 = () => {
-    toggleLoadingSpinner(true);
-    const searchField = document.getElementById('search-field2')
-    const searchText = searchField.value;
-    loadPhone(searchText);
-
-}
+// const handleSearch2 = () => {
+//     toggleLoadingSpinner(true);
+//     const searchField = document.getElementById('search-field2')
+//     const searchText = searchField.value;
+//     loadPhone(searchText);
+// }
 
 const toggleLoadingSpinner = (isLoading) =>{
     const loadingSpinner = document.getElementById('loading-spinner');
@@ -78,5 +79,10 @@ const toggleLoadingSpinner = (isLoading) =>{
         loadingSpinner.classList.add('hidden');
     }   
 }
+
+// handle show all 
+  const handleShowAll = () =>{
+    handleSearch(true);
+  }
 
 // loadPhone();
