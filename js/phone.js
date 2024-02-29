33c2ab083a7470df1740e0ce70fc2ef0d543da66
nahid <nahidrabbi045@gmail.com>
2024-02-29 06:15:34 +0600

    added modal part

--- js/phone.js
+++ js/phone.js
@@ -8,7 +8,7 @@ const loadPhone = async (searchText, isShowAll) => {
 
 
 const displayPhones = (phones, isShowAll) =>{
-        console.log(phones)
+        // console.log(phones)
 
      const phoneContainer = document.getElementById('phone-container');
     //  clear phone container cards before adding new cards
@@ -22,7 +22,7 @@ const displayPhones = (phones, isShowAll) =>{
     else{
         showAllContainer.classList.add('hidden');
     }
-     console.log('is show all', isShowAll)
+    //  console.log('is show all', isShowAll)
     // display only first 12 phones if not show all
     if(!isShowAll){
         phones = phones.slice(0,12);
@@ -39,9 +39,10 @@ const displayPhones = (phones, isShowAll) =>{
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
           <h2 class="card-title">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+          <p>The simplicity of Apple.
+          In a credit card.</p>
+          <div class="card-actions justify-center">
+            <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         `;
@@ -52,6 +53,35 @@ const displayPhones = (phones, isShowAll) =>{
     //  hide loading spinner
     toggleLoadingSpinner
     (false);
+}
+
+// 
+const handleShowDetail = async (id) =>{
+    // console.log('clicked show details', id)
+    // load single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+
+    showPhoneDetails(phone)
+} 
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+    showDetailContainer.innerHTML = `
+     <img src="${phone.image}" alt="" />
+     <p><span>storage:</span>${phone?.mainFeatures?.storage}</p>
+     <p><span>GPS:</span>${phone?.others?.GPS || 'No GPS available'}</p>
+     <p><span>GPS:</span>${phone?.others?.GPS ? phone.others.GPS:  'No GPS available in this device'}</p>
+    `
+
+
+    //    show the modal
+    show_details_modal.showModal();
 }
 
 // handle search button
